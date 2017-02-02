9d4ef49dbaf2942ed29c3f2794fb3c8034abd734
snicolo <seby.nicol@gmail.com>
2017-02-02 15:35:51 +0100

    admin fix

--- blogmaterial/settings.py
+++ blogmaterial/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'material.admin',
+    #'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
