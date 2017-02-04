22148df8dcc118150a2d9b5bcb51d2c6db6f0335
sebas <sebas@DESKTOP-1SG2L9V>
2017-02-04 16:12:08 +0100

    contact and about page

--- blogmaterial/urls.py
+++ blogmaterial/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from material.frontend import urls as frontend_urls
+from . import views
 
 urlpatterns = [
     # Examples:
@@ -9,4 +10,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+	url(r'^about/', views.about, name='about'),
+	url(r'^contact/', views.contact, name='contact'),
 ]
